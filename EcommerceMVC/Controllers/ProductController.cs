32f0b611d9a65e14a8211ed3f751eefa3eaffd33
duongse141129 <duong97498@gmail.com>
2024-11-06 01:03:12 +0700

    Show Product list

--- EcommerceMVC/Controllers/ProductController.cs
+++ EcommerceMVC/Controllers/ProductController.cs
@@ -1,12 +1,64 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EcommerceMVC.Data;
+using EcommerceMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EcommerceMVC.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly Hshop2023Context db;
+
+        public ProductController(Hshop2023Context context) 
+        {
+            db = context;
+        }
+
         public IActionResult Index(int? category)
         {
-            return View();
+            var products = db.Products.AsQueryable();
+
+            if(category.HasValue)
+            {
+                products = products.Where(p => p.CategoryId == category.Value);
+            }
+
+            var result = products.Select(p => new ProductVM
+            {
+                ProductId = p.ProductId,
+                ProductName = p.ProductName,
+                Price  = p.Price ?? 0,
+                ProductPhoto = p.ProductPhoto ?? "",
+                ShortDescription = p.Unit ?? "",
+                CategoryName = p.Category.CategoryName
+
+            });
+
+
+            return View(result);
+        }
+
+        public ActionResult Search(string? query)
+        {
+            var products = db.Products.AsQueryable();
+
+            if (query != null)
+            {
+                products = products.Where(p => p.ProductName.Contains(query));
+            }
+
+            var result = products.Select(p => new ProductVM
+            {
+                ProductId = p.ProductId,
+                ProductName = p.ProductName,
+                Price = p.Price ?? 0,
+                ProductPhoto = p.ProductPhoto ?? "",
+                ShortDescription = p.Unit ?? "",
+                CategoryName = p.Category.CategoryName
+
+            });
+
+
+            return View(result);
         }
     }
 }
---
+++ EcommerceMVC/ViewModels/ProductVM.cs
@@ -0,0 +1,13 @@
+﻿namespace EcommerceMVC.ViewModels
+{
+    public class ProductVM
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+        public string ProductPhoto { get; set; }
+        public double Price { get; set; }
+        public string ShortDescription { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
